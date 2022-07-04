7c141ac550875086203b3603b0e20fce0b62caaf
kdv1995 <tribeofdanel@gmail.com>
2022-07-04 22:07:19 +0300

    first__commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./App";
+import "./index.scss";
+import reportWebVitals from "./reportWebVitals";
+
+const defaultState = {
+  cash: 0,
+};
+
+const reducer = (state = defaultState, action) => {
+  console.log(action.type);
+  switch (action.type) {
+    case "ADD_CASH":
+      return { ...state, cash: state.cash + action.payload };
+
+    case "GET_CASH":
+      return { ...state, cash: state.cash - action.payload };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
