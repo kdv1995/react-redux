0694127ea8efb8dacaa6a914caf358fd6423b424
kdv1995 <tribeofdanel@gmail.com>
2022-07-04 22:20:29 +0300

    04/07/2022:22:20

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.scss";
 
-function App() {
-  return <div className="App">Hello world</div>;
-}
+const App = () => {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash);
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+  return (
+    <div className="App">
+      <div>{cash}</div>
+      <button onClick={() => addCash(Number(prompt()))}>Add Cash</button>
+      <button onClick={() => getCash(Number(prompt()))}>Get Cash</button>
+    </div>
+  );
+};
 
 export default App;
